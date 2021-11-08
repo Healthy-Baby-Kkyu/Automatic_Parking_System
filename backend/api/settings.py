b8206af359b289cac86b2748c825472e44d6a728
LeeTaeGyu <dnjsqls2008@gmail.com>
2021-11-08 17:44:06 +0900

    Add mysql, Customer table

--- backend/api/settings.py
+++ backend/api/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-u5)-!$$n(%l@r%_kr9(ofo%e_n8c4c8ysbpm8vnsp=#*mq8ke0
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -93,10 +93,17 @@ WSGI_APPLICATION = 'api.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': { 
+    	'ENGINE': 'django.db.backends.mysql', 
+        'NAME': 'miri_parking', 
+        'USER': 'root', 
+        'PASSWORD': '0000', 
+        'HOST': 'localhost', 
+        'PORT': '3306', 
+        'OPTIONS': {
+            'init_command': 'SET sql_mode="STRICT_TRANS_TABLES"'
+        }
+     } 
 }
 
 
@@ -124,7 +131,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Seoul'
 
 USE_I18N = True
 
--- backend/customer/models.py
+++ backend/customer/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Customer(models.Model):
+    user_id = models.CharField(max_length=100, unique=True)
+    user_name = models.TextField()
+    password = models.TextField()
+    birthday = models.DateTimeField()
+    phone_number = models.IntegerField()
+    point = models.IntegerField()
+    total_fee = models.IntegerField()
+
+    def __str__(self):
+        return [self.id ,self.name, self.descript]
