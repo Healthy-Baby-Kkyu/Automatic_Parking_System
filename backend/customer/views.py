3483ffd52253f5971ac299d6a91dbb5860370e11
LeeTaeGyu <dnjsqls2008@gmail.com>
2021-11-13 14:39:11 +0900

    Update views.py

--- backend/customer/views.py
+++ backend/customer/views.py
@@ -1,3 +1,27 @@
 from django.shortcuts import render
+from rest_framework import generics
+from rest_framework.serializers import Serializer
+from rest_framework.parsers import JSONParser
+
+
+from .models import User, Cars, ParkingSlot, Reservation
+from .serializers import UserSerializer, CarsSerializer, ParkingSlotSerializer, ReservationSerializer
+
+
+class CurrentUser():
+    id = ''
+    pw = ''
+    name = ''
+    
+    def __init__(self, id, pw, name):
+        self.id = id
+        self.pw = pw
+        self.name = name      
 
 # Create your views here.
+class Send(generics.ListCreateAPIView):
+    queryset = User.objects.filter(user_id='ddd')
+    serializer_class = UserSerializer
+
+    
+    
