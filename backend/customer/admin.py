59e638687f6e884acfe1cd421e27229024247647
LeeTaeGyu <dnjsqls2008@gmail.com>
2021-11-16 04:44:56 +0900

    Update admin.py

--- backend/customer/admin.py
+++ backend/customer/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import User
+from .models import User, Cars, Reservation, ParkingSlot
 
 admin.site.register(User)
+admin.site.register(Cars)
+admin.site.register(Reservation)
+admin.site.register(ParkingSlot)
